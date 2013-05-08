1d832b77b84fa2c7f2bbda2db20208555426e9eb
felix <felix@call-with-current-continuation.org>
2013-05-08 09:41:07 +0200

    added README and moved config.mk to default-config.mk.

---
+++ default-config.mk
@@ -0,0 +1,11 @@
+# Configuration for building a custom Cross-CHICKEN
+#
+# Copy this file to "config.mk" and make all necessary changes to reflect the configuration
+# of your environment.
+
+NDK_PATH         ?= /opt/google/android/ndk
+ANDROID_PLATFORM ?= android-14
+PACKAGE_NAME     ?= com.example.xyz
+HOST_ARCH        ?= linux-x86_64
+ARCH     	 ?= armeabi
+#DEBUGBUILD       ?= 1
